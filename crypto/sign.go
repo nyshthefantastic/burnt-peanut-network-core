8f1e77564772391c1a58a58a38e76236b4a8b095
jinali98 <jinalipabasara@gmail.com>
2026-02-22 19:48:06 +0530

    Initial commit

---
+++ crypto/sign.go
@@ -0,0 +1,37 @@
+package crypto
+
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"fmt"
+)
+
+
+ func GenerateKeyPair() (publicKey []byte, privateKey []byte, err error){
+	publicKey, privateKey, err = ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return publicKey, privateKey, nil
+ }
+
+
+
+func Sign(privateKey []byte, message []byte) (signature []byte, err error){
+	if len(privateKey) != ed25519.PrivateKeySize {
+		return nil, fmt.Errorf("expected key size %d, got %d", ed25519.PrivateKeySize, len(privateKey))
+	}
+	signature = ed25519.Sign(privateKey, message)
+	return signature, nil
+}
+
+func Verify(publicKey []byte, message []byte, signature []byte) (valid bool, err error) {
+	if len(publicKey) != ed25519.PublicKeySize {
+		return false, fmt.Errorf("expected key size %d, got %d", ed25519.PublicKeySize, len(publicKey))
+	}
+	
+	valid = ed25519.Verify(publicKey, message, signature)
+	
+	return valid, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nyshthefantastic/burnt-peanut-network-core
+
+go 1.25.7
