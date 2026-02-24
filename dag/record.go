0c8efd272e163e3b977e34ba9e166b48db3255cf
nyshthefantastic <astroanju80@yahoo.com>
2026-02-24 20:07:22 +0530

    validator functions for record dag implemented

--- dag/record.go
+++ dag/record.go
@@ -1,10 +1,90 @@
 package dag
 
 import (
+	"bytes"
 	"encoding/binary"
+	"fmt"
+
+	"github.com/nyshthefantastic/burnt-peanut-network-core/crypto"
 	"github.com/nyshthefantastic/burnt-peanut-network-core/wire/gen"
 )
 
+func ValidateFileMeta(r *gen.FileMeta) error {
+	message := FileMetaSignableBytes(r)
+	ok, err := crypto.Verify(r.OriginPubkey, message, r.OriginSig)
+	if err != nil {
+		return fmt.Errorf("Origin sig verification failed: %v", err)
+	}
+	if !ok {
+		return fmt.Errorf("Signature verification failed")
+	}
+	return nil
+}
+
+func ValidateTransferRequest(r *gen.TransferRequest) error {
+	message := TransferRequestSignableBytes(r)
+	ok, err := crypto.Verify(r.RequesterPubkey, message, r.Signature)
+	if err != nil {
+		return fmt.Errorf("Requester sig verification failed: %w", err)
+	}
+	if !ok {
+		return fmt.Errorf("invalid request signature")
+	}
+	return nil
+}
+
+func ValidateShareRecord(r *gen.ShareRecord) error {
+	message := SignableBytes(r)
+	ok, err := crypto.Verify(r.SenderPubkey, message, r.SenderSig)
+	if err != nil {
+		return fmt.Errorf("sender sig verification failed: %w", err)
+	}
+	if !ok {
+		return fmt.Errorf("invalid sender signature")
+	}
+	ok, err = crypto.Verify(r.ReceiverPubkey, message, r.ReceiverSig)
+
+	if err != nil {
+		return fmt.Errorf("receiver sig verification failed: %w", err)
+	}
+	if !ok {
+		return fmt.Errorf("invalid receiver signature")
+	}
+	expectedId := crypto.Hash(message)
+	if !bytes.Equal(expectedId[:], r.Id) {
+		return fmt.Errorf("record id mismatch")
+	}
+	return nil
+
+}
+
+func FileMetaSignableBytes(r *gen.FileMeta) []byte {
+	var buf []byte
+	buf = append(buf, r.FileHash...)
+	buf = append(buf, []byte(r.FileName)...)
+	buf = appendUint64(buf, r.FileSize)
+	buf = appendUint64(buf, r.ChunkSize)
+	buf = appendUint32(buf, uint32(len(r.ChunkHashes)))
+	for _, ch := range r.ChunkHashes {
+		buf = append(buf, ch...)
+	}
+	buf = append(buf, r.OriginPubkey...)
+	buf = appendUint64(buf, uint64(r.CreatedAt))
+	return buf
+}
+
+func TransferRequestSignableBytes(r *gen.TransferRequest) []byte {
+	var buf []byte
+	buf = append(buf, r.RequesterPubkey...)
+	buf = append(buf, r.FileHash...)
+	buf = appendUint32(buf, uint32(len(r.ChunkIndices)))
+	for _, idx := range r.ChunkIndices {
+		buf = appendUint32(buf, idx)
+	}
+	buf = append(buf, r.Nonce...)
+	buf = appendUint64(buf, uint64(r.Timestamp))
+	return buf
+}
 func SignableBytes(r *gen.ShareRecord) []byte {
 	var buf []byte
 	buf = append(buf, r.SenderPubkey...)
@@ -37,8 +117,16 @@ func SignableBytes(r *gen.ShareRecord) []byte {
 	return buf
 }
 
-func AttachSenderSig(r *gen.ShareRecord, sig []byte) []byte   {}
-func AttachReceiverSig(r *gen.ShareRecord, sig []byte) []byte {}
+func AttachSenderSig(r *gen.ShareRecord, sig []byte) {
+	r.SenderSig = sig
+}
+
+func AttachReceiverSig(r *gen.ShareRecord, sig []byte) {
+	r.ReceiverSig = sig
+	data := SignableBytes(r)
+	hash := crypto.Hash(data)
+	r.Id = hash[:]
+}
 
 func appendUint64(buf []byte, value uint64) []byte {
 	b := make([]byte, 8)
